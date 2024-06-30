050704889a025f26187496fbb59dc94b8528dbfa
skipperhoa <thanhhoacth2013@gmail.com>
2024-06-30 22:44:23 +0700

    v1.0.1-update check platform

--- app/(screens)/location.tsx
+++ app/(screens)/location.tsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, TouchableOpacity, TextInput, Image } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity, TextInput,Platform, Image } from 'react-native'
 import React from 'react'
 import { LinearGradient } from 'expo-linear-gradient'
 
@@ -6,77 +6,89 @@ import { LinearGradient } from 'expo-linear-gradient'
 import { Ionicons ,FontAwesome, Feather} from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
-
+// 📗 khai báo thư viện mà expo hổ trỡ để lấy giá trị chiều cao  statusBar
+import Constants from 'expo-constants';
 const LocationScreen = () => {
   const router = useRouter();
+  const checkPlatform = () => {
+    if (Platform.OS === "ios") {
+      return "ios";
+    } else {
+      return "android";
+    }
+  };
   return (
-    <LinearGradient colors={["#FC7533", "#FC7533", "#FA844A"]} className='w-full h-full'>
-      <SafeAreaView className='w-full h-full'>
-          <View className='w-full'>
-              {/* nav */}
-              <View className='w-full px-2 flex flex-row items-center'>
-                  <TouchableOpacity className='w-6' onPress={() => router.back()}>
-                        <Ionicons name="chevron-back" size={24} color="white" />
-                  </TouchableOpacity>
-                    <View className='w-auto flex-1'>
-                        <Text className='text-center text-white text-xl font-bold'>
-                            Recent Location
-                        </Text>
+    <>
+    
+      <LinearGradient colors={["#FC7533", "#FC7533", "#FA844A"]} className='w-full h-full'>
+      {checkPlatform() === "android" && <View style={{ height: Constants.statusBarHeight }} />}
+        <SafeAreaView className='w-full h-full'>
+            <View className='w-full'>
+                {/* nav */}
+                <View className='w-full px-2 flex flex-row items-center'>
+                    <TouchableOpacity className='w-6' onPress={() => router.back()}>
+                          <Ionicons name="chevron-back" size={24} color="white" />
+                    </TouchableOpacity>
+                      <View className='w-auto flex-1'>
+                          <Text className='text-center text-white text-xl font-bold'>
+                              Recent Location
+                          </Text>
+                  </View>
                 </View>
-              </View>
-              {/* search location */}
-               <View className='w-full pt-6'>
-                  <View className='w-full px-2 relative'>
-                     <TextInput className='w-full h-12 pl-10 pr-4 text-sm font-bold text-white bg-orange-400/100 rounded-full' placeholder='Search Location'
-                     placeholderTextColor = 'white'
-                     />
-                     <TouchableOpacity className='absolute left-[20px] top-[14px]'>
-                        <FontAwesome name="search" size={20} color="white" />
-                     </TouchableOpacity>
-                  </View>
-               </View>
-              
-          </View>
-           {/* map */}
-           <View className='w-full flex-1'>
-                  <View className='w-full h-full flex flex-col items-center justify-center'>
-                       <View className='w-[300px] h-[300px] border-[2px] border-dashed border-gray-100/40 rounded-full p-[50px] relative'>
-                             <View className='absolute right-10 top-0 z-10'><Feather name="map-pin" size={24} color="white" /></View>
-                             <View className='absolute left-10 bottom-6 z-10'><Feather name="map-pin" size={24} color="white" /></View>
-                             <View className='w-full relative z-20 top-1/2'>
-                                <View className='w-full absolute left-0 top-0 z-20'>
-                                    <TouchableOpacity className='w-full'>
-                                       <View className='w-full relative'>
-                                            <View className='bg-white w-full p-2 rounded-xl flex flex-row items-center justify-between'>
-                                                    <Image className='w-8 h-8 rounded-full' source={{uri:'https://hoanguyenit.com/images/hoanguyencoder.jpg'}} />
-                                                    <View className='w-auto flex-1 pl-4'>
-                                                        <Text className='text-sm font-bold'>Hoa Nguyen Coder</Text>
-                                                        <Text className='text-sm text-gray-500/100'>Ho Chi Minh</Text>
-                                                    </View>
-                                             </View>
-                                             <View className='w-full absolute left-0 -top-10'>
-                                                <View style={{transform:[{rotate:'-90deg'}]}} className='w-full flex flex-row items-center justify-between'>
-                                                    <MaterialIcons name="arrow-left" size={40} color="white" />
-                                                </View>
-                                             </View>
-                                             
-                                       </View>
-                                    </TouchableOpacity>
-                                </View>
-                             </View>
-                             <View className='w-full h-full border-[2px] border-dashed border-gray-100/40  rounded-full p-[50px]'>
-                                <View className='absolute left-0 top-0'><Feather name="map-pin" size={24} color="white" /></View>
-                                <View className='absolute -right-10 -bottom-16'><Feather name="map-pin" size={24} color="white" /></View>
-                                <View className='w-full h-full border-[2px] border-dashed border-gray-100/40 rounded-full relative'>
-                                    <View className='w-full h-full flex flex-col items-center justify-center relative top-20'><Feather name="map-pin" size={24} color="white" /></View>
-                                 
-                                </View>
-                            </View>
-                       </View>
-                  </View>
-               </View>
-      </SafeAreaView>
-    </LinearGradient>
+                {/* search location */}
+                <View className='w-full pt-6'>
+                    <View className='w-full px-2 relative'>
+                      <TextInput className='w-full h-12 pl-10 pr-4 text-sm font-bold text-white bg-orange-400/100 rounded-full' placeholder='Search Location'
+                      placeholderTextColor = 'white'
+                      />
+                      <TouchableOpacity className='absolute left-[20px] top-[14px]'>
+                          <FontAwesome name="search" size={20} color="white" />
+                      </TouchableOpacity>
+                    </View>
+                </View>
+                
+            </View>
+            {/* map */}
+            <View className='w-full flex-1'>
+                    <View className='w-full h-full flex flex-col items-center justify-center'>
+                        <View className='w-[300px] h-[300px] border-[2px] border-dashed border-gray-100/40 rounded-full p-[50px] relative'>
+                              <View className='absolute right-10 top-0 z-10'><Feather name="map-pin" size={24} color="white" /></View>
+                              <View className='absolute left-10 bottom-6 z-10'><Feather name="map-pin" size={24} color="white" /></View>
+                              <View className='w-full relative z-20 top-1/2'>
+                                  <View className='w-full absolute left-0 top-0 z-20'>
+                                      <TouchableOpacity className='w-full'>
+                                        <View className='w-full relative'>
+                                              <View className='bg-white w-full p-2 rounded-xl flex flex-row items-center justify-between'>
+                                                      <Image className='w-8 h-8 rounded-full' source={{uri:'https://hoanguyenit.com/images/hoanguyencoder.jpg'}} />
+                                                      <View className='w-auto flex-1 pl-4'>
+                                                          <Text className='text-sm font-bold'>Hoa Nguyen Coder</Text>
+                                                          <Text className='text-sm text-gray-500/100'>Ho Chi Minh</Text>
+                                                      </View>
+                                              </View>
+                                              <View className='w-full absolute left-0 -top-10'>
+                                                  <View style={{transform:[{rotate:'-90deg'}]}} className='w-full flex flex-row items-center justify-between'>
+                                                      <MaterialIcons name="arrow-left" size={40} color="white" />
+                                                  </View>
+                                              </View>
+                                              
+                                        </View>
+                                      </TouchableOpacity>
+                                  </View>
+                              </View>
+                              <View className='w-full h-full border-[2px] border-dashed border-gray-100/40  rounded-full p-[50px]'>
+                                  <View className='absolute left-0 top-0'><Feather name="map-pin" size={24} color="white" /></View>
+                                  <View className='absolute -right-10 -bottom-16'><Feather name="map-pin" size={24} color="white" /></View>
+                                  <View className='w-full h-full border-[2px] border-dashed border-gray-100/40 rounded-full relative'>
+                                      <View className='w-full h-full flex flex-col items-center justify-center relative top-20'><Feather name="map-pin" size={24} color="white" /></View>
+                                  
+                                  </View>
+                              </View>
+                        </View>
+                    </View>
+                </View>
+        </SafeAreaView>
+      </LinearGradient>
+    </>
   )
 }
 
--- app/(screens)/settings.tsx
+++ app/(screens)/settings.tsx
@@ -15,186 +15,197 @@ import { AntDesign } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
 import { Fontisto } from "@expo/vector-icons";
 import { EvilIcons } from "@expo/vector-icons";
-
+// 📗 khai báo thư viện mà expo hổ trỡ để lấy giá trị chiều cao  statusBar
+import Constants from 'expo-constants';
 const SettingScreen = () => {
   const [fontsLoaded, fontError] = useFonts({
     HelvetIns: require("../../assets/fonts/HelvetIns.ttf"),
     
   });
+  const checkPlatform = () => {
+    if (Platform.OS === "ios") {
+      return "ios";
+    } else {
+      return "android";
+    }
+  };
   return (
-    <LinearGradient colors={["#FC7325", "#FC813A"]} className="w-full h-full">
-      <SafeAreaView className="w-full h-full">
-        <View className="flex-1 w-full h-full">
-          <View className="w-full">
-            <View className="w-full pb-14">
-              <View className="w-full px-4 py-4">
-                <View className="flex flex-row items-center justify-between">
-                  <View className="flex flex-row items-center">
-                    <View className="w-20 h-20 rounded-full p-2 flex flex-col items-center justify-center bg-orange-100">
-                      <Image
-                        source={{
-                          uri: "https://hoanguyenit.com/images/hoanguyencoder.jpg",
-                        }}
-                        className="w-16 h-16 rounded-full"
-                        resizeMode="cover"
+    <>
+      <LinearGradient colors={["#FC7325", "#FC813A"]} className="w-full h-full">
+      {checkPlatform() === "android" && <View style={{ height: Constants.statusBarHeight }} />}
+        <SafeAreaView className="w-full h-full">
+          <View className="flex-1 w-full h-full">
+            <View className="w-full">
+              <View className="w-full pb-14">
+                <View className="w-full px-4 py-4">
+                  <View className="flex flex-row items-center justify-between">
+                    <View className="flex flex-row items-center">
+                      <View className="w-20 h-20 rounded-full p-2 flex flex-col items-center justify-center bg-orange-100">
+                        <Image
+                          source={{
+                            uri: "https://hoanguyenit.com/images/hoanguyencoder.jpg",
+                          }}
+                          className="w-16 h-16 rounded-full"
+                          resizeMode="cover"
+                        />
+                      </View>
+                      <View className="pl-4">
+                        <Text
+                          className="text-xl text-white"
+                          style={{ fontFamily: "HelvetIns" }}
+                        >
+                          Hoa Nguyen Coder
+                        </Text>
+                        <Text className="text-sm text-white pt-1">
+                          Web Developer
+                        </Text>
+                      </View>
+                    </View>
+                    <TouchableOpacity className="">
+                      <MaterialIcons
+                        name="navigate-next"
+                        size={30}
+                        color="white"
                       />
-                    </View>
-                    <View className="pl-4">
-                      <Text
-                        className="text-xl text-white"
-                        style={{ fontFamily: "HelvetIns" }}
-                      >
-                        Hoa Nguyen Coder
-                      </Text>
-                      <Text className="text-sm text-white pt-1">
-                        Web Developer
-                      </Text>
-                    </View>
+                    </TouchableOpacity>
                   </View>
+                </View>
+              </View>
+              <View className="w-full px-4 h-full bg-gray-100">
+                <View className="w-full bg-white rounded-2xl p-4 shadow-lg shadow-gray-300 relative -top-12">
+                  <Text className="w-full py-2 px-4 text-xl text-gray-500">
+                    Login and Security
+                  </Text>
                   <TouchableOpacity className="">
-                    <MaterialIcons
-                      name="navigate-next"
-                      size={30}
-                      color="white"
-                    />
+                    <View className="flex flex-row items-center py-5 bg-white">
+                      <View className="w-1/5 m-auto flex flex-col items-center justify-center">
+                        <AntDesign name="user" size={30} color="#FC6B18" />
+                      </View>
+                      <View className="w-4/5 flex-row items-center justify-between">
+                        <Text className="text-xl text-black">Username</Text>
+                        <TouchableOpacity className="">
+                          <MaterialIcons
+                            name="navigate-next"
+                            size={30}
+                            color="#707070"
+                          />
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  </TouchableOpacity>
+                  <TouchableOpacity className="">
+                    <View className="flex flex-row items-center py-5 bg-white">
+                      <View className="w-1/5 m-auto flex flex-col items-center justify-center">
+                        <Feather name="phone" size={24} color="#FC6B18" />
+                      </View>
+                      <View className="w-4/5 flex-row items-center justify-between">
+                        <Text className="text-xl text-black">Phone Number</Text>
+                        <TouchableOpacity className="">
+                          <MaterialIcons
+                            name="navigate-next"
+                            size={30}
+                            color="#707070"
+                          />
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity className="">
+                    <View className="flex flex-row items-center py-5 bg-white">
+                      <View className="w-1/5 m-auto flex flex-col items-center justify-center">
+                        <Fontisto name="email" size={24} color="#FC6B18" />
+                      </View>
+                      <View className="w-4/5 flex-row items-center justify-between">
+                        <Text className="text-xl text-black">Email</Text>
+                        <TouchableOpacity className="">
+                          <MaterialIcons
+                            name="navigate-next"
+                            size={30}
+                            color="#707070"
+                          />
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  </TouchableOpacity>
+                  <TouchableOpacity className="">
+                    <View className="flex flex-row items-center py-5 bg-white">
+                      <View className="w-1/5 m-auto flex flex-col items-center justify-center">
+                        <MaterialIcons
+                          name="password"
+                          size={24}
+                          color="#FC6B18"
+                        />
+                      </View>
+                      <View className="w-4/5 flex-row items-center justify-between">
+                        <Text className="text-xl text-black">Password</Text>
+                        <TouchableOpacity className="">
+                          <MaterialIcons
+                            name="navigate-next"
+                            size={30}
+                            color="#707070"
+                          />
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  </TouchableOpacity>
+
+                  <Text className="w-full py-2 px-4 text-xl text-gray-500">
+                    Data and Permission
+                  </Text>
+                  <TouchableOpacity className="">
+                    <View className="flex flex-row items-center py-5 bg-white">
+                      <View className="w-1/5 m-auto flex flex-col items-center justify-center">
+                        <MaterialIcons
+                          name="password"
+                          size={24}
+                          color="#FC6B18"
+                        />
+                      </View>
+                      <View className="w-4/5 flex-row items-center justify-between">
+                        <Text className="text-xl text-black">Location</Text>
+                        <TouchableOpacity className="">
+                          <MaterialIcons
+                            name="navigate-next"
+                            size={30}
+                            color="#707070"
+                          />
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  </TouchableOpacity>
+                  <TouchableOpacity className="">
+                    <View className="flex flex-row items-center py-5 bg-white">
+                      <View className="w-1/5 m-auto flex flex-col items-center justify-center">
+                        <AntDesign name="appstore-o" size={24} color="#FC6B18" />
+                      </View>
+                      <View className="w-4/5 flex-row items-center justify-between">
+                        <Text className="text-xl text-black">
+                          Apps and sessions
+                        </Text>
+                        <TouchableOpacity className="">
+                          <MaterialIcons
+                            name="navigate-next"
+                            size={30}
+                            color="#707070"
+                          />
+                        </TouchableOpacity>
+                      </View>
+                    </View>
                   </TouchableOpacity>
                 </View>
               </View>
             </View>
-            <View className="w-full px-4 h-full bg-gray-100">
-              <View className="w-full bg-white rounded-2xl p-4 shadow-lg shadow-gray-300 relative -top-12">
-                <Text className="w-full py-2 px-4 text-xl text-gray-500">
-                  Login and Security
-                </Text>
-                <TouchableOpacity className="">
-                  <View className="flex flex-row items-center py-5 bg-white">
-                    <View className="w-1/5 m-auto flex flex-col items-center justify-center">
-                      <AntDesign name="user" size={30} color="#FC6B18" />
-                    </View>
-                    <View className="w-4/5 flex-row items-center justify-between">
-                      <Text className="text-xl text-black">Username</Text>
-                      <TouchableOpacity className="">
-                        <MaterialIcons
-                          name="navigate-next"
-                          size={30}
-                          color="#707070"
-                        />
-                      </TouchableOpacity>
-                    </View>
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity className="">
-                  <View className="flex flex-row items-center py-5 bg-white">
-                    <View className="w-1/5 m-auto flex flex-col items-center justify-center">
-                      <Feather name="phone" size={24} color="#FC6B18" />
-                    </View>
-                    <View className="w-4/5 flex-row items-center justify-between">
-                      <Text className="text-xl text-black">Phone Number</Text>
-                      <TouchableOpacity className="">
-                        <MaterialIcons
-                          name="navigate-next"
-                          size={30}
-                          color="#707070"
-                        />
-                      </TouchableOpacity>
-                    </View>
-                  </View>
-                </TouchableOpacity>
-
-                <TouchableOpacity className="">
-                  <View className="flex flex-row items-center py-5 bg-white">
-                    <View className="w-1/5 m-auto flex flex-col items-center justify-center">
-                      <Fontisto name="email" size={24} color="#FC6B18" />
-                    </View>
-                    <View className="w-4/5 flex-row items-center justify-between">
-                      <Text className="text-xl text-black">Email</Text>
-                      <TouchableOpacity className="">
-                        <MaterialIcons
-                          name="navigate-next"
-                          size={30}
-                          color="#707070"
-                        />
-                      </TouchableOpacity>
-                    </View>
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity className="">
-                  <View className="flex flex-row items-center py-5 bg-white">
-                    <View className="w-1/5 m-auto flex flex-col items-center justify-center">
-                      <MaterialIcons
-                        name="password"
-                        size={24}
-                        color="#FC6B18"
-                      />
-                    </View>
-                    <View className="w-4/5 flex-row items-center justify-between">
-                      <Text className="text-xl text-black">Password</Text>
-                      <TouchableOpacity className="">
-                        <MaterialIcons
-                          name="navigate-next"
-                          size={30}
-                          color="#707070"
-                        />
-                      </TouchableOpacity>
-                    </View>
-                  </View>
-                </TouchableOpacity>
-
-                <Text className="w-full py-2 px-4 text-xl text-gray-500">
-                  Data and Permission
-                </Text>
-                <TouchableOpacity className="">
-                  <View className="flex flex-row items-center py-5 bg-white">
-                    <View className="w-1/5 m-auto flex flex-col items-center justify-center">
-                      <MaterialIcons
-                        name="password"
-                        size={24}
-                        color="#FC6B18"
-                      />
-                    </View>
-                    <View className="w-4/5 flex-row items-center justify-between">
-                      <Text className="text-xl text-black">Location</Text>
-                      <TouchableOpacity className="">
-                        <MaterialIcons
-                          name="navigate-next"
-                          size={30}
-                          color="#707070"
-                        />
-                      </TouchableOpacity>
-                    </View>
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity className="">
-                  <View className="flex flex-row items-center py-5 bg-white">
-                    <View className="w-1/5 m-auto flex flex-col items-center justify-center">
-                      <AntDesign name="appstore-o" size={24} color="#FC6B18" />
-                    </View>
-                    <View className="w-4/5 flex-row items-center justify-between">
-                      <Text className="text-xl text-black">
-                        Apps and sessions
-                      </Text>
-                      <TouchableOpacity className="">
-                        <MaterialIcons
-                          name="navigate-next"
-                          size={30}
-                          color="#707070"
-                        />
-                      </TouchableOpacity>
-                    </View>
-                  </View>
-                </TouchableOpacity>
-              </View>
-            </View>
           </View>
-        </View>
-        <TouchableOpacity className="w-full p-4 mt-2">
-            <View className=" flex flex-row items-center justify-center bg-white rounded-xl py-4 ">
-                <AntDesign name="logout" size={24} color="red" />
-                <Text className="text-red-500  tex-center text-xl px-4">Deactivate</Text>
-            </View>
-        </TouchableOpacity>
-      </SafeAreaView>
-    </LinearGradient>
+          <TouchableOpacity className="w-full p-4 mt-2">
+              <View className=" flex flex-row items-center justify-center bg-white rounded-xl py-4 ">
+                  <AntDesign name="logout" size={24} color="red" />
+                  <Text className="text-red-500  tex-center text-xl px-4">Deactivate</Text>
+              </View>
+          </TouchableOpacity>
+        </SafeAreaView>
+      </LinearGradient>
+    </>
   );
 };
 
