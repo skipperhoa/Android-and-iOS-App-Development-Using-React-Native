27547802e236b58f74938a788476fe7a53806226
skipperhoa <thanhhoacth2013@gmail.com>
2024-07-12 08:48:47 +0700

    v1.0.2

--- app/(screens)/(tabs)/_layout.tsx
+++ app/(screens)/(tabs)/_layout.tsx
@@ -1,15 +1,53 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text ,StyleSheet,Dimensions, Image} from "react-native";
+import React,{useState} from "react";
 import { Tabs } from "expo-router";
+import { getPathDown } from "../../../components/curve";
+import { Svg, Path } from "react-native-svg";
+import { scale } from "react-native-size-scaling";
+
+
 // ICON
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+
+const CustomTitleTab = ({ children } : any) => {
+  return (
+    <View style={styles.tabTitle}>
+      <Text style={styles.tabTitleText}>{children}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  tabTitle: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tabTitleText: {
+    fontSize: 14,   
+    color: "white",
+    paddingVertical: 10,
+    fontWeight: "bold",
+  },
+});
+
 const TabRootLayout = () => {
+  const [maxWidth, setMaxWidth] = useState(Dimensions.get("window").width);
+  const returnpathDown = getPathDown(maxWidth, 60, 50);
   return (
     <>
       <Tabs
         screenOptions={{
             headerShown: false,
-            tabBarActiveTintColor: "green",
+            tabBarActiveTintColor: "white",
+            tabBarInactiveTintColor: "white",
+            tabBarStyle: {
+              backgroundColor: "green",
+              borderTopWidth: 0,
+              position: "absolute",
+              elevation: 0,
+            },
         }}
       >
         <Tabs.Screen
@@ -18,8 +56,9 @@ const TabRootLayout = () => {
             title: "Home",
             
             tabBarIcon: ({ color, focused }) => {
-              return <MaterialCommunityIcons name="home" size={24} color={color} />
+              return <MaterialCommunityIcons name="home" size={30} color={color} />
             },
+            tabBarLabel: () => <CustomTitleTab>Home</CustomTitleTab>,
            
           }}
         
@@ -30,8 +69,47 @@ const TabRootLayout = () => {
           options={{
             title: "favorites",
             tabBarIcon: ({ color, focused }) => {
-                return <MaterialCommunityIcons name="heart" size={24} color={color} />
-              }
+                return <MaterialCommunityIcons name="heart" size={30} color={color} />
+              },
+              tabBarLabel: () => <CustomTitleTab>favorites</CustomTitleTab>,
+           
+          }}
+        />
+        <Tabs.Screen
+          name="qrcode"
+          options={{
+            headerShown: false,
+            unmountOnBlur: false,
+            tabBarItemStyle: {
+              margin: 0,
+              backgroundColor: "white",
+              zIndex: -50,
+
+            },
+            tabBarIcon: () => (
+              <View
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: 60,
+                  width: 60,
+                  backgroundColor: "green",
+                  borderRadius: 30,
+              
+                  
+                }}
+              >
+                <AntDesign name="qrcode" size={30} color="white" />
+              </View>
+            ),
+            tabBarLabel: () => (
+              <View>
+                <Svg width={maxWidth} height={scale(60)}>
+                  <Path fill={"green"} {...{ d: returnpathDown }} />
+                </Svg>
+              </View>
+            ),
           }}
         />
         <Tabs.Screen
@@ -39,8 +117,9 @@ const TabRootLayout = () => {
           options={{
             title: "Messages",
             tabBarIcon: ({ color, focused }) => {
-                return <MaterialCommunityIcons name="message" size={24} color={color} />
-              }
+                return <MaterialCommunityIcons name="message" size={30} color={color} />
+              },
+              tabBarLabel: () => <CustomTitleTab>Messages</CustomTitleTab>,
           }}
         />
           <Tabs.Screen
@@ -48,8 +127,9 @@ const TabRootLayout = () => {
           options={{
             title: "settings",
             tabBarIcon: ({ color, focused }) => {
-                return <MaterialCommunityIcons name="cog" size={24} color={color} />
-              }
+                return <MaterialCommunityIcons name="cog" size={30} color={color} />
+              },
+              tabBarLabel: () => <CustomTitleTab>settings</CustomTitleTab>,
           }}
         />
       </Tabs>
---
+++ app/(screens)/(tabs)/qrcode.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const QrcodeScreen = () => {
+  return (
+    <View>
+      <Text>QrcodeScreen</Text>
+    </View>
+  )
+}
+
+export default QrcodeScreen
