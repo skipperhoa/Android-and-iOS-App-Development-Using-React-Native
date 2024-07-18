289ac764b83d2783b119bf26397653ac142931c2
skipperhoa <thanhhoacth2013@gmail.com>
2024-07-18 09:25:03 +0700

    v1.0.1 - add Animated

--- app/(screens)/(tabs)/_layout.tsx
+++ app/(screens)/(tabs)/_layout.tsx
@@ -1,16 +1,28 @@
-import { View, Text ,StyleSheet,Dimensions, Image,TouchableOpacity} from "react-native";
-import React,{useState} from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+  Animated,
+} from "react-native";
+import React, { useState, useRef, useEffect } from "react";
 import { Tabs } from "expo-router";
 import { getPathDown } from "../../../components/curve";
 import { Svg, Path } from "react-native-svg";
 import { scale } from "react-native-size-scaling";
-import { getHeaderTitle } from '@react-navigation/elements';
+import { getHeaderTitle } from "@react-navigation/elements";
 
 // ICON
-import { MaterialCommunityIcons ,Ionicons,EvilIcons} from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
+import {
+  MaterialCommunityIcons,
+  Ionicons,
+  EvilIcons,
+} from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 // 📗 khai báo thư viện mà expo hổ trỡ để lấy giá trị chiều cao  statusBar
-import Constants from 'expo-constants';
+import Constants from "expo-constants";
 // custom header
 // const MyHeader = ({title,style} : any) => {
 //   console.warn(style)
@@ -19,7 +31,7 @@ import Constants from 'expo-constants';
 //   </View>
 // }
 
-const CustomTitleTab = ({ children } : any) => {
+const CustomTitleTab = ({ children }: any) => {
   return (
     <View style={styles.tabTitle}>
       <Text style={styles.tabTitleText}>{children}</Text>
@@ -27,69 +39,132 @@ const CustomTitleTab = ({ children } : any) => {
   );
 };
 
-
 const TabRootLayout = () => {
   const [maxWidth, setMaxWidth] = useState(Dimensions.get("window").width);
   const heightHeader = Constants.statusBarHeight + 50;
   const returnpathDown = getPathDown(maxWidth, 60, 50);
+
+  const translation = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    const animation = Animated.loop(
+      Animated.sequence([ 
+        Animated.timing(translation, {
+          toValue: 60, // chiều cao của nút qrcode
+          duration: 1000,
+          useNativeDriver: false,
+        }),
+        Animated.timing(translation, {
+          toValue: 1, // bắt đầu từ 0 đến 60
+          duration: 1000, // số giây
+          useNativeDriver: false,
+        }),
+      ])
+    );
+    animation.start(); // chạy cái function animation
+
+    // xoá animation sau khi chạy xong ->"unmount"
+    return () => animation.stop();
+  }, [translation]); // Nếu translation thay đổi , thì nó sẽ chạy lại animation
+
   return (
     <>
       <Tabs
         screenOptions={{
-            headerShown: true, // kaka
-            tabBarActiveTintColor: "white",
-            tabBarInactiveTintColor: "white",
-            tabBarStyle: {
-              backgroundColor: "green",
-              borderTopWidth: 0,
-              position: "absolute",
-              elevation: 0,
-            },
-            header: ({ navigation, route, options }) => {
+          headerShown: true, // kaka
+          tabBarActiveTintColor: "white",
+          tabBarInactiveTintColor: "white",
+          tabBarStyle: {
+            backgroundColor: "green",
+            borderTopWidth: 0,
+            position: "absolute",
+            elevation: 0,
+          },
+          header: ({ navigation, route, options }) => {
+            // lấy thông tin name của tab
+            // tìm hiểu thêm : https://reactnavigation.org/docs/bottom-tab-navigator/
+            // https://reactnavigation.org/docs/elements/#header
 
-              // lấy thông tin name của tab
-              // tìm hiểu thêm : https://reactnavigation.org/docs/bottom-tab-navigator/
-              // https://reactnavigation.org/docs/elements/#header
+            const title = getHeaderTitle(options, route.name);
 
-              const title = getHeaderTitle(options, route.name);
-              
-              // check kiểm tra tab hiện tại có phải là "Home" hay không
-              if(title==="Home"){
-                  return <>
-                      <View style={{width:'100%',height:heightHeader, backgroundColor:'green',paddingTop:Constants.statusBarHeight}}>
-                          {/* header */}
-                          <View style={styles.headerTop}>
-                              <View>
-                                  <View style={styles.headerContent}>
-                                      <TouchableOpacity>
-                                          <Ionicons name="filter" size={35} color="white" />
-                                      </TouchableOpacity>
-                                      <View style={{flexShrink:1,flex:1, justifyContent:'center', alignItems:'center'}}>
-                                          <Text style={[styles.headerTitle,{fontFamily: "HelvetIns", fontSize: 30}]}>Travel App</Text>
-                                      </View>
-                                      <TouchableOpacity>
-                                        <View>
-                                            <EvilIcons name="bell" size={40} color="white" />
-                                            <View style={{width:22,height:22,borderRadius:20,backgroundColor:'red',position:'absolute',top:-5,right:0,flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
-                                                <Text style={{color:'white',textAlign:'center',fontSize:10,fontWeight:'bold'}}>99</Text>
-                                            </View>
-                                        </View>
-                                      </TouchableOpacity>
-                                  </View>
-                              </View>
+            // check kiểm tra tab hiện tại có phải là "Home" hay không
+            if (title === "Home") {
+              return (
+                <>
+                  <View
+                    style={{
+                      width: "100%",
+                      height: heightHeader,
+                      backgroundColor: "green",
+                      paddingTop: Constants.statusBarHeight,
+                    }}
+                  >
+                    {/* header */}
+                    <View style={styles.headerTop}>
+                      <View>
+                        <View style={styles.headerContent}>
+                          <TouchableOpacity>
+                            <Ionicons name="filter" size={35} color="white" />
+                          </TouchableOpacity>
+                          <View
+                            style={{
+                              flexShrink: 1,
+                              flex: 1,
+                              justifyContent: "center",
+                              alignItems: "center",
+                            }}
+                          >
+                            <Text
+                              style={[
+                                styles.headerTitle,
+                                { fontFamily: "HelvetIns", fontSize: 30 },
+                              ]}
+                            >
+                              Travel App
+                            </Text>
                           </View>
+                          <TouchableOpacity>
+                            <View>
+                              <EvilIcons name="bell" size={40} color="white" />
+                              <View
+                                style={{
+                                  width: 22,
+                                  height: 22,
+                                  borderRadius: 20,
+                                  backgroundColor: "red",
+                                  position: "absolute",
+                                  top: -5,
+                                  right: 0,
+                                  flexDirection: "row",
+                                  justifyContent: "center",
+                                  alignItems: "center",
+                                }}
+                              >
+                                <Text
+                                  style={{
+                                    color: "white",
+                                    textAlign: "center",
+                                    fontSize: 10,
+                                    fontWeight: "bold",
+                                  }}
+                                >
+                                  99
+                                </Text>
+                              </View>
+                            </View>
+                          </TouchableOpacity>
+                        </View>
                       </View>
-                  
-                  </>
-              }
-
-              // chúng ta có thể cấu hình cho các tab khác nếu chúng ta muốn
-              else{
-                  return null
-              }
-            
-              
+                    </View>
+                  </View>
+                </>
+              );
             }
+
+            // chúng ta có thể cấu hình cho các tab khác nếu chúng ta muốn
+            else {
+              return null;
+            }
+          },
         }}
       >
         <Tabs.Screen
@@ -98,23 +173,24 @@ const TabRootLayout = () => {
             title: "Home",
             // headerStyle:styles.headerStyle,
             tabBarIcon: ({ color, focused }) => {
-              return <MaterialCommunityIcons name="home" size={30} color={color} />
+              return (
+                <MaterialCommunityIcons name="home" size={30} color={color} />
+              );
             },
             tabBarLabel: () => <CustomTitleTab>Home</CustomTitleTab>,
-           
           }}
-        
         />
-        
-          <Tabs.Screen
+
+        <Tabs.Screen
           name="favorites"
           options={{
             title: "favorites",
             tabBarIcon: ({ color, focused }) => {
-                return <MaterialCommunityIcons name="heart" size={30} color={color} />
-              },
-              tabBarLabel: () => <CustomTitleTab>favorites</CustomTitleTab>,
-           
+              return (
+                <MaterialCommunityIcons name="heart" size={30} color={color} />
+              );
+            },
+            tabBarLabel: () => <CustomTitleTab>favorites</CustomTitleTab>,
           }}
         />
         <Tabs.Screen
@@ -126,7 +202,6 @@ const TabRootLayout = () => {
               margin: 0,
               backgroundColor: "white",
               zIndex: -50,
-
             },
             tabBarIcon: () => (
               <View
@@ -138,11 +213,22 @@ const TabRootLayout = () => {
                   width: 60,
                   backgroundColor: "green",
                   borderRadius: 30,
-              
-                  
+                  overflow: "hidden",
                 }}
               >
                 <AntDesign name="qrcode" size={30} color="white" />
+                <Animated.View
+                  style={[
+                    {
+                      position: "absolute",
+                      right: 0,
+                      width: 60,
+                      height: 1,
+                      backgroundColor: "white",
+                    },
+                    { top: translation },
+                  ]}
+                ></Animated.View>
               </View>
             ),
             tabBarLabel: () => (
@@ -159,19 +245,27 @@ const TabRootLayout = () => {
           options={{
             title: "Messages",
             tabBarIcon: ({ color, focused }) => {
-                return <MaterialCommunityIcons name="message" size={30} color={color} />
-              },
-              tabBarLabel: () => <CustomTitleTab>Messages</CustomTitleTab>,
+              return (
+                <MaterialCommunityIcons
+                  name="message"
+                  size={30}
+                  color={color}
+                />
+              );
+            },
+            tabBarLabel: () => <CustomTitleTab>Messages</CustomTitleTab>,
           }}
         />
-          <Tabs.Screen
+        <Tabs.Screen
           name="settings"
           options={{
             title: "settings",
             tabBarIcon: ({ color, focused }) => {
-                return <MaterialCommunityIcons name="cog" size={30} color={color} />
-              },
-              tabBarLabel: () => <CustomTitleTab>settings</CustomTitleTab>,
+              return (
+                <MaterialCommunityIcons name="cog" size={30} color={color} />
+              );
+            },
+            tabBarLabel: () => <CustomTitleTab>settings</CustomTitleTab>,
           }}
         />
       </Tabs>
@@ -181,7 +275,7 @@ const TabRootLayout = () => {
 
 const styles = StyleSheet.create({
   headerStyle: {
-    backgroundColor:'green',
+    backgroundColor: "green",
     height: 80, // Specify the height of your custom header
   },
   tabTitle: {
@@ -190,28 +284,26 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   tabTitleText: {
-    fontSize: 14,   
+    fontSize: 14,
     color: "white",
     paddingVertical: 10,
     fontWeight: "bold",
   },
   headerTop: {
-    width: '100%',
+    width: "100%",
     paddingHorizontal: 20,
-  
-   
   },
-  headerContent:{
-    display: 'flex',
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'space-between',
+  headerContent: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
-  headerTitle:{
-    fontSize:18,
-    fontWeight:'600',
-    color:'#fff'
+  headerTitle: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#fff",
   },
-})
+});
 
 export default TabRootLayout;
