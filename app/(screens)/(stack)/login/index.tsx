18b6e642258850fc5e5f2bc98c9a6087ac95ee80
HoaCode <hoacode@192.168.0.107>
2025-03-14 10:39:01 +0700

    update code login screen

--- app/(screens)/(stack)/login/index.tsx
+++ app/(screens)/(stack)/login/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TouchableOpacity, Alert, TextInput } from "react-native";
+import { View, Text, TouchableOpacity, Alert, TextInput , Image,StyleSheet} from "react-native";
 import { useRouter } from "expo-router";
 import {
   EvilIcons,
@@ -10,7 +10,7 @@ import {
   FontAwesome,
   MaterialCommunityIcons,
 } from "@expo/vector-icons";
-
+import { useFonts } from "expo-font";
 // 📗 khai báo thư viện mà expo hổ trỡ để lấy giá trị chiều cao  statusBar
 import Constants from "expo-constants";
 import { LinearGradient } from "expo-linear-gradient";
@@ -18,49 +18,41 @@ import { LinearGradient } from "expo-linear-gradient";
 const MAX_HEIGHT_HEADER = 200;
 export default function LoginScreen() {
   const router = useRouter();
+   const [fontsLoaded, fontError] = useFonts({
+      HelvetIns: require("@/assets/fonts/HelvetIns.ttf"),
+      PlaywriteNL: require("@/assets/fonts/Playwrite_NL/Playwrite-NL.ttf"),
+      Montserrat: require("@/assets/fonts/Montserrat/static/Montserrat-Regular.ttf"),
+    });
   return (
     <>
-      {/* header */}
-      <View
-        className="w-full absolute z-50"
-        style={{ height: MAX_HEIGHT_HEADER }}
-      >
-        <View
-          className="w-full bg-gray-100 pb-4"
-          style={{ paddingTop: Constants.statusBarHeight + 10 }}
-        >
-          <View className="w-full flex-row items-center justify-between px-2">
-            <TouchableOpacity onPress={() => router.back()}>
-               <View className="w-[35px] h-[35px] rounded-full flex-row items-center justify-center bg-white">
-                  <AntDesign name="arrowleft" size={20} color="black" />
-               </View>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-
+    
       {/* content */}
       <View
-        className="w-full h-full bg-gray-100"
-        style={{ paddingTop: Constants.statusBarHeight + 70 }}
+        className="w-full h-full bg-blue-600"
+        style={{ paddingTop: Constants.statusBarHeight + 50 }}
       >
         {/* form login */}
         <View className="w-full">
-          <Text className="font-bold text-2xl text-black uppercase text-center">
-            ĐĂNG NHẬP
-          </Text>
+          <View className="w-[100px] h-[100px] rounded-full bg-blue-400 flex-col items-center justify-center m-auto">
 
-          {/* info form */}
-          <Text className="w-full text-center text-[14px] text-gray-500 pt-3 px-4">
-            Chào mừng bạn đã quay trở lại, chúng tôi rất nhớ bạn!
+          <View className="w-[80px] h-[80px] rounded-full bg-blue-300 flex-col items-center justify-center m-auto">
+
+          <View className="w-[60px] h-[60px] rounded-full bg-blue-200 flex-col items-center justify-center m-auto">
+            <Image
+            source={require("@/assets/images/xeoto.png")} className="w-[40px] h-[40px] mx-auto" />
+          </View>
+          </View>
+          </View>
+          <Text className="font-bold text-2xl text-white uppercase text-center mt-4" style={{fontFamily: "HelvetIns"}}>
+            ĐĂNG NHẬP
           </Text>
 
           {/* form group */}
 
-          <View className="w-full px-5 pt-10">
+          <View className="w-full px-5 pt-2">
             {/* item */}
             <View className="w-full py-2">
-              <Text className="w-full font-bold text-gray-600 py-2">Email</Text>
+              <Text className="w-full text-white py-2" style={{fontFamily: "Montserrat"}}>Email</Text>
               <TextInput
                 className="w-full bg-white p-5 rounded-md"
                 placeholder="nguyen.thanh.hoa.ctec@gmail.com"
@@ -69,7 +61,7 @@ export default function LoginScreen() {
 
             {/* item */}
             <View className="w-full py-2">
-              <Text className="w-full font-bold text-gray-600 py-2">
+              <Text className="w-full text-white py-2"  style={{fontFamily: "Montserrat"}}>
                 Password
               </Text>
               <View className="w-full relative">
@@ -79,7 +71,7 @@ export default function LoginScreen() {
                   secureTextEntry={true}
                 />
                 <TouchableOpacity
-                  className="absolute right-5 top-3"
+                  className="absolute right-5 top-[14px]"
                   onPress={() => {
                     Alert.alert("Eye");
                   }}
@@ -91,14 +83,14 @@ export default function LoginScreen() {
 
             <View>
                 <TouchableOpacity>
-                      <Text className="w-full text-gray-600 py-2 text-right italic underline">Quên mật khẩu?</Text>
+                      <Text className="w-full text-white py-2 text-right italic underline"  style={{fontFamily: "Montserrat"}}>Quên mật khẩu?</Text>
                 </TouchableOpacity>
             </View>
 
             {/* button */}
             <View className="w-full py-2">
                  <LinearGradient 
-                    colors={['#01b1fa', '#1f73fc', '#337ef9']} style={{width:'100%',borderRadius:50,padding:13}}>
+                    colors={['#0396e5', '#0352e5', '#032de5']} style={{width:'100%',borderRadius:50,padding:13}}>
                         <TouchableOpacity className="w-full">
                             <View className="w-fullflex flex-row items-center justify-center gap-2">
                                 <AntDesign name="arrowright" size={24} color="white" />
@@ -114,24 +106,24 @@ export default function LoginScreen() {
           {/* Group social */}
           <View className="w-full px-5 pt-2">
               <View className="w-full flex flex-row items-center justify-center gap-2">
-                  <Text className="text-sm text-gray-500">Bạn chưa có tài khoản?</Text>
+                  <Text className="text-sm text-white"  style={{fontFamily: "Montserrat"}}>Bạn chưa có tài khoản?</Text>
                   <TouchableOpacity>
-                      <Text className="w-full text-blue-500 py-2 text-right font-bold">Đăng ký</Text>
+                      <Text className="w-full text-white py-2 text-right font-bold"  style={{fontFamily: "Montserrat"}}>Đăng ký</Text>
                     </TouchableOpacity>
               </View>
 
               {/* box social */}
               <View className="w-full pt-20">
                   <TouchableOpacity className="w-full">
-                      <View className="w-full flex flex-row items-center justify-center gap-3  bg-white py-5 rounded-xl">
-                      <AntDesign name="google" size={24} color="black" />
-                         <Text className="text-gray-700">Đăng nhập bằng Google</Text>
+                      <View className="w-full flex flex-row items-center justify-center gap-3  bg-white py-5 rounded-xl" style={styles.boxShadow}>
+                         <Image source={require("@/assets/images/google.png")} style={{width:30,height:30}} />
+                         <Text className="text-gray-700"  style={{fontFamily: "Montserrat"}}>Đăng nhập bằng Google</Text>
                       </View>
                   </TouchableOpacity>
                   <TouchableOpacity className="w-full mt-4">
-                      <View className="w-full flex flex-row items-center justify-center gap-3 bg-[#055cc2] py-5 rounded-xl">
-                      <AntDesign name="facebook-square" size={24} color="white" />
-                         <Text className="text-white">Đăng nhập bằng Facebook</Text>
+                      <View className="w-full flex flex-row items-center justify-center gap-3 bg-white py-5 rounded-xl" style={styles.boxShadow}>
+                      <Image source={require("@/assets/images/facebook.png")} style={{width:30,height:30}} />
+                         <Text className="text-black"  style={{fontFamily: "Montserrat"}}>Đăng nhập bằng Facebook</Text>
                       </View>
                   </TouchableOpacity>
               </View>
@@ -143,3 +135,16 @@ export default function LoginScreen() {
     </>
   );
 }
+
+const styles = StyleSheet.create({
+    boxShadow: {
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 1,
+        },
+        shadowOpacity: 0.6,
+        shadowRadius: 3.84,
+        elevation: 5,
+      },
+})
